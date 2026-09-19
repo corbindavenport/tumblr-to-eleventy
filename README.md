6d6b8871a9c58814a5351c0fad82eb4506295699
Corbin Davenport <corbindavenport@outlook.com>
2026-09-19 18:49:53 -0400

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# tumblr-to-eleventy
-Export Tumblr blogs into Eleventy-friendly Markdown format.
+# Tumblr to Eleventy Migration Tool
+
+A PowerShell script for migrating Tumblr blog posts to an [Eleventy website](https://www.11ty.dev/), while retaining their original permalinks, titles, tags, and other metadata. The output format for posts is HTML, cleaned up with [Pandoc](https://pandoc.org).
+
+**Note:** This was only tested with my personal blog's ~80 posts, running on macOS.
+
+### How to use
+
+You need [PowerShell 7 or later](https://learn.microsoft.com/en-us/powershell/scripting/install/install-powershell) and [Pandoc](https://pandoc.org). You can install them with [Homebrew](https://brew.sh/) on macOS like this:
+
+```shell
+brew install powershell pandoc
+```
+
+You also need an [export of your Tumblr blog](https://help.tumblr.com/knowledge-base/export-your-blog/). When it's ready, download the ZIP file and place it in the directory where you are running the script.
+
+Next, register an [API application with Tumblr](https://corbin.io). You can put anything in the title, callback URL, and other required fields.
+
+Finally, run the script like this:
+
+```shell
+./main.ps1 -key YourKeyGoesHere -blog myblog.tumblr.com -tag "post"
+```
+
+Your OAuth Consumer Key from your application's API page goes in the `key` parameter, and your blog's domain or other [valid blog identifier](https://www.tumblr.com/docs/en/api/v2#blog-identifiers) in the `blog` parameter. The `tag` parameter is optional, but you can use it to apply a certain tag to all exported posts. For example, if you're already using a "post" tag for all your blog posts, you should add that to the command.
+
+The posts will be saved in an `exports` directory. You can move the contents to any location in your Eleventy site's structure.
---
+++ main.ps1
@@ -0,0 +1,112 @@
+#Requires -Version 7.0
+
+param (
+    [Parameter(Mandatory = $true)][string]$key,
+    [Parameter(Mandatory = $true)][string]$blog,
+    [Parameter(Mandatory = $false)][string]$tag
+)
+
+[int]$RetrievedPosts = 0
+[bool]$ContinueSearching = $true
+
+function Get-Tumblr-Posts {
+    Param(
+        [Parameter(Mandatory = $true)][string]$ApiKey,
+        [Parameter(Mandatory = $true)][string]$BlogUrl,
+        [Parameter(Mandatory = $true)][int]$Offset
+    )
+
+    $Req = "https://api.tumblr.com/v2/blog/$BlogUrl/posts?api_key=$Key&npf=false&reblog_info=false&limit=40&offset=$Offset"
+    $Data = Invoke-RestMethod -Method 'Get' -Uri $Req
+    $Data | ConvertTo-Json -Depth 50 | Out-File "./responses/list_$RetrievedPosts.json" -Encoding UTF8
+
+    $BlogName = $Data.response.blog.title
+    $BlogUrl = $Data.response.blog.url
+    $PostCount = $Data.response.posts.length
+    
+    Write-Host "Fetched $PostCount posts from $BlogName ($BlogUrl)"
+    return $Data
+}
+
+# Check that ZIP download is present
+if (-not(Test-Path "*.zip")) {
+    Write-Host "Can't find Tumblr export ZIP! It should be here: $(Join-Path -Path $pwd -ChildPath 'example.zip')"
+    exit
+}
+
+# Unzip Tumblr ZIP download
+if (-not(Test-Path "./import/*.zip")) {
+    $TumblrZip = Get-ChildItem -Path (Join-Path -Path $pwd -ChildPath '*.zip') | Select-Object -First 1
+    Expand-Archive -Path $TumblrZip -DestinationPath "./import"
+    Write-Host "Extracted Tumblr archive to this directory: $(Join-Path -Path $pwd -ChildPath 'import')"
+}
+
+# Unzip posts
+if (-not(Test-Path "./import/posts/")) {
+    Expand-Archive -Path "./import/posts.zip" -DestinationPath "./import/posts"
+    Write-Host "Extracted posts from Tumblr archive to this directory: $(Join-Path -Path $pwd -ChildPath 'import/posts')"
+}
+
+# Create folder for JSON responses
+$null = New-Item -ItemType Directory -Force -Path "responses"
+
+# Copy media directory to export folder
+Copy-Item -Path "./import/media" -Destination "./export/tumblr_media" -Recurse -Force
+Write-Host "Copied media to export directory"
+
+# Run search and iterate through all pages
+while ($ContinueSearching -eq $true) {
+    $ThisPage = Get-Tumblr-Posts -ApiKey $key -BlogUrl $blog -Offset $RetrievedPosts
+    if ($ThisPage.response.posts -and $ThisPage.response.posts.items) {
+        $RetrievedPosts += $ThisPage.response.posts.Length
+        foreach ($Post in $ThisPage.response.posts) {
+            # Create output directory
+            $null = New-Item -ItemType Directory -Force -Path "export/$($Post.slug)"
+            # Get title of the post
+            if ($Post.title) {
+                $PostTitle = $Post.title
+            } else {
+                $PostTitle = $Post.summary
+            }
+            # Get tags for post
+            if ($tag) {
+                $TagsString = "$tag, $($Post.tags -join ", ")"
+            } else {
+                $TagsString = $Post.tags -join ", "
+            }
+            # Clean up HTML with Pandoc
+            $OriginalPath = Join-Path -Path $pwd -ChildPath "import/posts/html/$($Post.id).html"
+            $TargetPath = Join-Path -Path $pwd -ChildPath "export/$($Post.slug)/$($Post.slug).html"
+            $Pandoc = & pandoc $OriginalPath -f html -t html --ascii=true --wrap=none
+            $Html = $Pandoc | Out-String
+            # Replace media paths
+            $Html = $Html -replace '../../media/', '../tumblr_media/'
+            # Remove "More" divider
+            $Html = $Html -replace '<p>\[\[MORE\]\]</p>', ''
+            # Remove "ALT" button under images
+            $Html = $Html -replace '<span class="tmblr-alt-text-helper">ALT</span>', ""
+            # Remove empty headers
+            $Html = $Html -replace '<h1 id="section"></h1>', ''
+            # Remove footers
+            $Html = $Html -replace '<div id="footer"[\s\S]*?<\/div>', ''
+            # Write file
+            $PostContents = @"
+---
+title: $PostTitle
+permalink: "$($Post.post_url -replace $Post.blog.url, "")"
+date: $($Post.date)
+tags: $TagsString
+id: $($Post.id)
+---
+
+$Html
+"@
+            $PostContents | Out-File -FilePath $TargetPath -Force
+            Write-Host "Finished converting: $($PostTitle) ($($Post.date))"
+        }
+    }
+    else {
+        $ContinueSearching = $false
+        Write-Host "Finished search with $RetrievedPosts posts."
+    }
+}
